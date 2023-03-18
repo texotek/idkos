3597de846f133d419d5c9b69a534a8fe6ae73f6d
texotek <fdahmen@proton.me>
2023-03-18 13:13:06 +0100

    booting with fat and reading from disk works

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,12 +1,16 @@
-org 0x7c00
+; Bootloader for IDKOS
+; Author Felix Dahmen (texotek)
+;
+
+org 0x7C00
 bits 16
 
-%define endl 0xd, 0xa
+%define ENDL 0x0D, 0x0A
 
-jmp short main
+; FAT12 headers
+jmp short start
 nop
 
-; FAT HEADERS
 dfh_oem_name:             db "IDKOS0.1"     ; 8 bytes
 dfh_bytes:                dw 512
 dfh_sectors_per_cluster:  db 1
@@ -22,35 +26,197 @@ dfh_hidden_sectors:       dd 0
 dfh_large_sector_count:   dd 0
 
 ; extended boot record
-ebr_drive_number:       db 0                ; 0x00 floppy
-                        db 0
-ebr_signature:          db 0x29             
-ebr_volume_id:          dd 0x12,0x34,0x56,0x78       ; Serial number
-ebr_volume_label:       db "IDKOS      "    ; 11 bytes
-ebr_volume_type:        db "FAT12   "       ; 8 bytes
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number
+ebr_volume_label:           db 'IDKOS      '        ; 11 bytes, padded with spaces
+ebr_system_id:              db 'FAT12   '           ; 8 bytes
 
+; Code goes here
+start:
+    jmp main
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+    
 
 main:
-DISK_NUM: db 0
-mov [DISK_NUM], dl
+    ; setup data segments
+    mov ax, 0                   ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
+    
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00              ; stack grows downwards from where we are loaded in memory
 
-mov ah, 2 ; Read from disk
-mov al, 1 ; Read one sector
+    ; read something from floppy disk
+    ; BIOS should set DL to drive number
+    mov [ebr_drive_number], dl
 
-mov bx, 0x7e00 ; Where to read to
+    mov ax, 1                   ; LBA=1, second sector from disk
+    mov cl, 1                   ; 1 sector to read
+    mov bx, 0x7E00              ; data should be after the bootloader
+    call disk_read
 
-mov ch, 0 ; cylinder 0
-mov cl, 2 ; Sector 2
-mov dh, 0 ; Head 0
-mov dl, DISK_NUM ; Disk number
+    cli                         ; disable interrupts, this way CPU can't get out of "halt" state
+    hlt
 
-int 0x13 ; Interrupt 0x13
 
-mov ah, 0xe ; Print
-mov al, [0x7e00]
-int 0x10
+;
+; Error handlers
+;
 
-jmp $
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h                     ; wait for keypress
+    jmp 0FFFFh:0                ; jump to beginning of BIOS, should reboot
+
+.halt:
+    cli                         ; disable interrupts, this way CPU can't get out of "halt" state
+    hlt
+
+
+;
+; Disk routines
+;
+
+;
+; Converts an LBA address to a CHS address
+; Parameters:
+;   - ax: LBA address
+; Returns:
+;   - cx [bits 0-5]: sector number
+;   - cx [bits 6-15]: cylinder
+;   - dh: head
+;
+
+lba_to_chs:
+
+    push ax
+    push dx
+
+    xor dx, dx                          ; dx = 0
+    div word [dfh_sectors_per_track]    ; ax = LBA / SectorsPerTrack
+                                        ; dx = LBA % SectorsPerTrack
+
+    inc dx                              ; dx = (LBA % SectorsPerTrack + 1) = sector
+    mov cx, dx                          ; cx = sector
+
+    xor dx, dx                          ; dx = 0
+    div word [dfh_heads]                ; ax = (LBA / SectorsPerTrack) / Heads = cylinder
+                                        ; dx = (LBA / SectorsPerTrack) % Heads = head
+    mov dh, dl                          ; dh = head
+    mov ch, al                          ; ch = cylinder (lower 8 bits)
+    shl ah, 6
+    or cl, ah                           ; put upper 2 bits of cylinder in CL
+
+    pop ax
+    mov dl, al                          ; restore DL
+    pop ax
+    ret
+
+
+;
+; Reads sectors from a disk
+; Parameters:
+;   - ax: LBA address
+;   - cl: number of sectors to read (up to 128)
+;   - dl: drive number
+;   - es:bx: memory address where to store read data
+;
+disk_read:
+
+    push ax                             ; save registers we will modify
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx                             ; temporarily save CL (number of sectors to read)
+    call lba_to_chs                     ; compute CHS
+    pop ax                              ; AL = number of sectors to read
+    
+    mov ah, 02h
+    mov di, 3                           ; retry count
+
+.retry:
+    pusha                               ; save all registers, we don't know what bios modifies
+    stc                                 ; set carry flag, some BIOS'es don't set it
+    int 13h                             ; carry flag cleared = success
+    jnc .done                           ; jump if carry not set
+
+    ; read failed
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    ; all attempts are exhausted
+    jmp floppy_error
+
+.done:
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax                             ; restore registers modified
+    ret
+
+
+;
+; Resets disk controller
+; Parameters:
+;   dl: drive number
+;
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+
+
+msg_read_failed:        db 'Read from disk failed!', ENDL, 0
 
 times 510-($-$$) db 0
-dw 0xaa55
+dw 0AA55h
