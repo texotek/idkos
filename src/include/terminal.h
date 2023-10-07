bf0f59d743e697d1411d7cd15ceaf2cdd602532f
texotek <fdahmen@proton.me>
2023-10-07 21:21:37 +0200

    update that i forgot to push

--- src/include/terminal.h
+++ src/include/terminal.h
@@ -1,10 +1,8 @@
 #ifndef INCLUDE_TERMINAL_H
 #define INCLUDE_TERMINAL_H
 
-void putc(unsigned char c);
-void move_cursor(unsigned short position);
 void terminal_init(void);
-void write_string(const char* string);
+void putc(unsigned char c);
 void printf(const char* format, ...);
 
 #endif
--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,7 @@
-#include <stdint.h>
 #include <terminal.h>
 
-void kmain(void) 
+void kmain(void)
 {
-    char * p = "Du bimbo\n\n\n\n\n\n\ndfgdsfgdfgdfsg\n\nsdfg\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\ndfafd";
     terminal_init();
-    printf("%s", p);
+    printf("%i", 3);
 }
